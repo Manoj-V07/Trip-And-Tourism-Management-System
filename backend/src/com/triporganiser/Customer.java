9f8611a2b6b69d72af207e8546222cee49bf0c33
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-09-20 15:25:30 +0530

    Updated Customer

--- backend/src/com/triporganiser/Customer.java
+++ backend/src/com/triporganiser/Customer.java
@@ -1,36 +1,7 @@
 package com.triporganiser;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Customer extends User {
-    private List<Booking> bookings;
-
-    public Customer(int userId, String username, String password, String name, String email) {
-        super(userId, username, password, name, email, "customer");
-        bookings = new ArrayList<>();
-    }
-
-    public void addBooking(Booking booking) {
-        if (booking != null) bookings.add(booking);
-    }
-
-    public void cancelBooking(int bookingId) {
-        bookings.removeIf(b -> b.getBookingId() == bookingId);
-    }
-
-    public List<Booking> getBookings() {
-        return new ArrayList<>(bookings);
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "userId=" + getUserId() +
-                ", username='" + getUsername() + '\'' +
-                ", name='" + getName() + '\'' +
-                ", email='" + getEmail() + '\'' +
-                ", bookingsCount=" + bookings.size() +
-                '}';
+    public Customer(int userId, String name, String username, String password) {
+        super(userId, name, username, password);
     }
 }
