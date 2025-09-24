c21469cd7b87ecb7b9600b6714c60211d65d4b29
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-09-24 17:20:50 +0530

    Commented the User class

--- backend/src/com/triporganiser/User.java
+++ backend/src/com/triporganiser/User.java
@@ -1,25 +1,34 @@
 package com.triporganiser;
 
-public abstract class User {
+public abstract class User { 
     private int userId;
     private String name;
     private String username;
     private String password;
 
-    public User(int userId, String name, String username, String password) {
+    /* Constructor used to initialize the object */
+    public User(int userId , String name , String username , String password){
         this.userId = userId;
         this.name = name;
         this.username = username;
         this.password = password;
     }
 
+    /* Getter Methods For userId , name , username , password to 
+    return when the respective method is called */
     public int getUserId() { return userId; }
-    public String getName() { return name; }
-    public String getUsername() { return username; }
-    public String getPassword() { return password; }
 
+    public String getName() { return name; }
+
+    public String getUsername() { return username; }
+
+    public String getPassword()  {return password; }
+
+    /* This default toString function is used to representing 
+    the string in customized form*/
     @Override
-    public String toString() {
-        return "User ID: " + userId + ", Name: " + name + ", Username: " + username;
+    public String toString(){
+        return "User ID : " + userId + ", Name : " + name + 
+        ", Username : " + username;
     }
 }
