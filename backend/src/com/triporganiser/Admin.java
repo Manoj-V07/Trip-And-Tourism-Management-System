1ed6db51e4b01bdc612effeda29b025430523f56
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-09-24 17:01:51 +0530

    Commented the Admin File

--- backend/src/com/triporganiser/Admin.java
+++ backend/src/com/triporganiser/Admin.java
@@ -1,7 +1,8 @@
-package com.triporganiser;
+package com.triporganiser;    
 
-public class Admin extends User {
-    public Admin(int userId, String name, String username, String password) {
-        super(userId, name, username, password);
+public class Admin extends User{                /* Admin Class is inherrited from the User Class */
+
+    public Admin(int userId , String name , String username , String password){
+        super(userId , name , username , password);  /* Calls the constuctor of the User Calss and initializes the Admin Object */
     }
 }
