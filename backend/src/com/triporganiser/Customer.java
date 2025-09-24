c54cb1fca35d9df26c5072ed953faf3e68719102
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-09-24 17:08:00 +0530

    Commented the Customer Class

--- backend/src/com/triporganiser/Customer.java
+++ backend/src/com/triporganiser/Customer.java
@@ -1,7 +1,8 @@
 package com.triporganiser;
 
-public class Customer extends User {
-    public Customer(int userId, String name, String username, String password) {
-        super(userId, name, username, password);
+public class Customer extends User{  /* Customer Class Is Inherited From User Class*/
+
+    public Customer(int userId , String name , String username , String password){
+        super(userId , name , username , password);    /* Calls Constructor Of User Class And Intialize The Customer Object */
     }
 }
