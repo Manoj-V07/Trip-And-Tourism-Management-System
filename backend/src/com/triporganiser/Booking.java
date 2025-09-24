7d3c5a02b6674bec29437f7feabcb922227286fa
Manoj-V07 <manoj.v2024cse@sece.ac.in>
2025-09-24 17:03:32 +0530

    Commented the Booking Class

--- backend/src/com/triporganiser/Booking.java
+++ backend/src/com/triporganiser/Booking.java
@@ -1,22 +1,34 @@
 package com.triporganiser;
 
 public class Booking {
+
+    /* This private instance variable are used to 
+    store the states of the Booking Object  */
     private int bookingId;
     private int userId;
     private int packageId;
 
-    public Booking(int bookingId, int userId, int packageId) {
+    /* Constructor method  is called to create or initialize 
+    the Booking Object with the given parameters */
+    public Booking(int bookingId , int userId , int packageId){
         this.bookingId = bookingId;
         this.userId = userId;
         this.packageId = packageId;
     }
 
-    public int getBookingId() { return bookingId; }
+    /* Getter Method To Return The Booking ID */
+    public int getBookingId() { return bookingId; } 
+
+    /* Getter Method To Return The User ID */
     public int getUserId() { return userId; }
+
+    /* Getter Method To Return The Package ID */
     public int getPackageId() { return packageId; }
 
+    /* Overrides the default toString method to provide a string representation customly */
     @Override
-    public String toString() {
-        return "Booking ID: " + bookingId + ", User ID: " + userId + ", Package ID: " + packageId;
+    public String toString(){
+        return "Booking ID: " + bookingId + ", User ID : " + userId +
+        ", Package ID : " + packageId;
     }
 }
